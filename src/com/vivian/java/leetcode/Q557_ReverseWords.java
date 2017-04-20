abe9d295ef7d44a977ec49e1587159d7a40338b5
George.Zhi.Xu <George.Zhi.Xu@gmail.com>
2017-04-20 11:26:25 -0700

    Add Q557

---
+++ src/com/vivian/java/leetcode/Q557_ReverseWords.java
@@ -0,0 +1,18 @@
+package com.vivian.java.leetcode;
+
+/**
+ * Given a string, you need to reverse the order of characters in each word
+ * within a sentence while still preserving whitespace and initial word order.
+ * 
+ * Example 1:
+ * 
+ * Input: "Let's take LeetCode contest"
+ * <p>
+ * Output: "s'teL ekat edoCteeL tsetnoc"
+ * 
+ * Note: In the string, each word is separated by single space and there will
+ * not be any extra space in the string.
+ */
+public class Q557_ReverseWords {
+
+}
