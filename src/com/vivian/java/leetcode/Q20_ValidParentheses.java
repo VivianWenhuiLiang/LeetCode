38ec0d9edb4d649592f0d957bf986295d6113a4e
George.Zhi.Xu <George.Zhi.Xu@gmail.com>
2017-04-21 00:03:50 -0700

    Add Q20

---
+++ src/com/vivian/java/leetcode/Q20_ValidParentheses.java
@@ -0,0 +1,12 @@
+package com.vivian.java.leetcode;
+
+/**
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ * 
+ * The brackets must close in the correct order, "()" and "()[]{}" are all valid
+ * but "(]" and "([)]" are not.
+ */
+public class Q20_ValidParentheses {
+
+}
