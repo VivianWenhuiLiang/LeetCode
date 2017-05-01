bafc0c2a045d67642d7961f39e3c574bdc70b72d
George.Zhi.Xu <xuzhi@amazon.com>
2017-04-30 19:08:44 -0700

    Add Q14, 20, 38

---
+++ src/com/vivian/java/leetcode/Q38_CountandSay.java
@@ -0,0 +1,14 @@
+package com.vivian.java.leetcode;
+
+/**
+ * The count-and-say sequence is the sequence of integers beginning as follows:
+ * 1, 11, 21, 1211, 111221, ...
+ * 
+ * 1 is read off as "one 1" or 11. 11 is read off as "two 1s" or 21. 21 is read
+ * off as "one 2, then one 1" or 1211.
+ * 
+ * Given an integer n, generate the nth sequence.
+ */
+public class Q38_CountandSay {
+
+}
