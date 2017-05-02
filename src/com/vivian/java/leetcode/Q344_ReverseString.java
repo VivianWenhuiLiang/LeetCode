24811914d503761debbf6cedf7c043647044fe33
George.Zhi.Xu <xuzhi@amazon.com>
2017-05-01 21:02:58 -0700

    Add Q344, 345 and 521

---
+++ src/com/vivian/java/leetcode/Q344_ReverseString.java
@@ -0,0 +1,11 @@
+package com.vivian.java.leetcode;
+
+/**
+ * Write a function that takes a string as input and returns the string
+ * reversed.
+ * 
+ * Example: Given s = "hello", return "olleh".
+ */
+public class Q344_ReverseString {
+
+}
