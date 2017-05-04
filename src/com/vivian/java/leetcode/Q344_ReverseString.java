4c19e385a97f711a8916066bad72a5095ebad2f4
vivian <Vivian.Wenhui.Liang@gmail.com>
2017-05-04 09:31:14 -0700

    Q344 done

--- src/com/vivian/java/leetcode/Q344_ReverseString.java
+++ src/com/vivian/java/leetcode/Q344_ReverseString.java
@@ -7,5 +7,17 @@ package com.vivian.java.leetcode;
  * Example: Given s = "hello", return "olleh".
  */
 public class Q344_ReverseString {
+    public String reverseString(String str) {
+        if (str.isEmpty()||str == null) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder(str);
+        for (int i = 0, j = sb.length() - 1; i < j; i++, j--) {
+            char temp = sb.charAt(i);
+            sb.setCharAt(i, sb.charAt(j));
+            sb.setCharAt(j, temp);
+        }
+        return sb.toString();
+    }
 
 }
