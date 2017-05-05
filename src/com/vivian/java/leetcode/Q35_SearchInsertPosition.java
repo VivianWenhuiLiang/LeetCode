380462fd56be0410dd60f6544df46bc474e1e10f
George.Zhi.Xu <xuzhi@amazon.com>
2017-05-04 21:28:19 -0700

    Add Q35, 53, 66

---
+++ src/com/vivian/java/leetcode/Q35_SearchInsertPosition.java
@@ -0,0 +1,18 @@
+package com.vivian.java.leetcode;
+
+/**
+ * Given a sorted array and a target value, return the index if the target is
+ * found. If not, return the index where it would be if it were inserted in
+ * order.
+ * 
+ * You may assume no duplicates in the array.
+ * 
+ * Here are few examples. <br>
+ * [1,3,5,6], 5 → 2 <br>
+ * [1,3,5,6], 2 → 1 <br>
+ * [1,3,5,6], 7 → 4 <br>
+ * [1,3,5,6], 0 → 0 <br>
+ */
+public class Q35_SearchInsertPosition {
+
+}
