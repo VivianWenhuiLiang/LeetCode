32e6b356984b9c8ebcdc48e737419d9b5bfa36e1
George.Zhi.Xu <xuzhi@amazon.com>
2017-05-09 11:39:38 -0700

    Add Q167, 169, 189, 217, 219

---
+++ src/com/vivian/java/leetcode/Q217_ContainsDuplicate.java
@@ -0,0 +1,10 @@
+package com.vivian.java.leetcode;
+
+/**
+ * Given an array of integers, find if the array contains any duplicates. Your
+ * function should return true if any value appears at least twice in the array,
+ * and it should return false if every element is distinct.
+ */
+public class Q217_ContainsDuplicate {
+
+}
