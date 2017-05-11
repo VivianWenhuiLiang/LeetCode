9fc5f1e8bdde0a82434f1a8f5d442ef331cdf220
vivian <Vivian.Wenhui.Liang@gmail.com>
2017-05-10 20:51:12 -0700

    Q169 @1

--- src/com/vivian/java/leetcode/Q169_MajorityElement.java
+++ src/com/vivian/java/leetcode/Q169_MajorityElement.java
@@ -1,5 +1,8 @@
 package com.vivian.java.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given an array of size n, find the majority element. The majority element is
  * the element that appears more than ⌊ n/2 ⌋ times.
@@ -8,5 +11,20 @@ package com.vivian.java.leetcode;
  * exist in the array.
  */
 public class Q169_MajorityElement {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+                if (map.get(nums[i]) > nums.length / 2) {
+                    return nums[i];
+                }
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        throw new IllegalArgumentException();
+
+    }
 
 }
