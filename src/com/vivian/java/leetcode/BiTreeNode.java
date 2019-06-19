468b7b76b5f0f9f3ed9ef01086ef25a213fe08b3
wenhui <vivian.wenhui.liang@gmail.com>
2019-06-19 13:12:36 -0700

    June19ArrayToBST

---
+++ src/com/vivian/java/leetcode/BiTreeNode.java
@@ -0,0 +1,28 @@
+package com.vivian.java.leetcode;
+
+public class BiTreeNode<T> {
+    private T data;
+    private BiTreeNode<T> left;
+    private BiTreeNode<T> right;
+
+    public BiTreeNode(T data, BiTreeNode<T> left, BiTreeNode<T> right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T item) {
+        data = item;
+    }
+    public BiTreeNode<T> getLeftNode() {
+        return left;
+    }
+    public BiTreeNode<T> getRightNode() {
+        return right;
+    }
+
+}
